9781196730cc7eda2b9310e3366b9cf623e4c92a
YodaNicolas <nicolasyoda3@gmail.com>
2023-11-28 03:51:57 -0800

    page admin 87%

--- Styles/Js/calculeTempReel.js
+++ Styles/Js/calculeTempReel.js
@@ -2,7 +2,7 @@
 function Calculer() {
     // Récupérer les valeurs saisies
     var total = 0;
-    for (var i = 1; i <= 2; i++) {
+    for (var i = 1; i <= 4; i++) {
         var qte = parseInt(document.getElementById(-i).value) || 0;
         if (qte < 0) {
             alert("Vous avez saisi une valeur négative à la ligne N°" + i + " veuillez corriger si non cette ligne sera ignoré et ne figurera pas dans votre commande MERCI.");
@@ -18,7 +18,7 @@ function Calculer() {
 }
 
 // Ajouter un gestionnaire d'événement à chaque input
-for (var i = 1; i <= 2; i++) {
+for (var i = 1; i <= 4; i++) {
     document.getElementById(-i).addEventListener("input", Calculer);
 }
 
